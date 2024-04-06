136942f83ac9e75a9e2e3523f74b0833c68863fe
ElgatoS2 <165394571+ElgatoS2@users.noreply.github.com>
2024-04-06 18:33:08 +0100

    Update chain.tsx

--- pages/chain.tsx
+++ pages/chain.tsx
@@ -1,10 +1,10 @@
-import { Chain } from "@thirdweb-dev/chains";
+import { Chain, ChainId } from "@thirdweb-dev/chains";
 
 const CustomChain: Chain = {
   chain: "bitrock",
   name: "bitrock",
   chainId: 7171,
-  rpc: ["https://connect.bit-rock.io"], // Change this to an array
+  rpc: ["https://connect.bit-rock.io"], // Changed to an array
   nativeCurrency: {
     name: "BROCK",
     symbol: "BROCK",
@@ -15,4 +15,4 @@ const CustomChain: Chain = {
   slug: "bitrock",
 };
 
-export default CustomChain;
+export { CustomChain, ChainId };
